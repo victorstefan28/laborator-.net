47370f9cc4a96cc16c2fe395c53541ea97abe0ce
Victor Cucu <victorstefan28@gmail.com>
2023-10-12 10:19:40 +0300

    tema 1 - clasa student si materie

---
+++ Lab1/LAB_DAW/Models/Student.cs
@@ -0,0 +1,29 @@
+﻿namespace Models
+{
+    public class Student
+    {
+        private string Nume { get; set; }
+        private string Prenume { get; set; }
+
+        private string Clasa { get; set; }
+
+
+        public List<Materie> Materii { get; set; } = new List<Materie>();
+
+        public Student(string nume, string prenume)
+        {
+            Nume = nume;
+            Prenume = prenume;
+        }
+
+        public void AfisareMaterii()
+        {
+            Console.WriteLine($"Student: {Nume} {Prenume}");
+            Console.WriteLine("Materii:");
+            foreach (var materie in Materii)
+            {
+                Console.WriteLine($"- {materie.Titlu}, Durata: {materie.Durata}");
+            }
+        }
+    }
+}
---
+++ Lab1/LAB_DAW/Program.cs
@@ -0,0 +1,32 @@
+﻿using Models;
+
+
+class Program
+{
+    static void Main()
+    {
+        Materie materie1 = new Materie("Matematica", "30 ore");
+        Materie materie2 = new Materie("Fizica", "25 ore");
+        Materie materie3 = new Materie("Informatica", "40 ore");
+
+        List<Student> students = new List<Student>();
+
+        Student student1 = new Student("Andrei", "Ion");
+        student1.Materii.Add(materie1);
+        student1.Materii.Add(materie2);
+
+        Student student2 = new Student("Stefan", "Bogdan");
+        student2.Materii.Add(materie2);
+        student2.Materii.Add(materie3);
+
+        students.Add(student1);
+        students.Add(student2);
+
+
+        foreach (var item in students)
+        {
+            item.AfisareMaterii();
+            Console.WriteLine();
+        }
+    }
+}
